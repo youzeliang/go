6095b8d5c588c76c21a9ff683fa443567eca78bb
youze <youzel@126.com>
2020-05-05 10:04:13 +0800

    read hashmap

--- src/runtime/map.go
+++ src/runtime/map.go
@@ -115,15 +115,23 @@ func isEmpty(x uint8) bool {
 type hmap struct {
 	// Note: the format of the hmap is also encoded in cmd/compile/internal/gc/reflect.go.
 	// Make sure this stays in sync with the compiler's definition.
-	count     int // # live cells == size of map.  Must be first (used by len() builtin)
-	flags     uint8
-	B         uint8  // log_2 of # of buckets (can hold up to loadFactor * 2^B items)
+	// 元素个数，调用 len(map) 时，直接返回此值
+	count int // # live cells == size of map.  Must be first (used by len() builtin)
+	flags uint8
+	// buckets 的对数 log_2
+	B uint8 // log_2 of # of buckets (can hold up to loadFactor * 2^B items)
+	// overflow 的 bucket 近似数
 	noverflow uint16 // approximate number of overflow buckets; see incrnoverflow for details
-	hash0     uint32 // hash seed
+	// 计算 key 的哈希的时候会传入哈希函数
+	hash0 uint32 // hash seed
 
-	buckets    unsafe.Pointer // array of 2^B Buckets. may be nil if count==0.
+	// 指向 buckets 数组，大小为 2^B
+	// 如果元素个数为0，就为 nil
+	buckets unsafe.Pointer // array of 2^B Buckets. may be nil if count==0.
+	// 扩容的时候，buckets 长度会是 oldbuckets 的两倍
 	oldbuckets unsafe.Pointer // previous bucket array of half the size, non-nil only when growing
-	nevacuate  uintptr        // progress counter for evacuation (buckets less than this have been evacuated)
+	// 指示扩容进度，小于此地址的 buckets 迁移完成
+	nevacuate uintptr // progress counter for evacuation (buckets less than this have been evacuated)
 
 	extra *mapextra // optional fields
 }
